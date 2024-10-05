980035475892d558174434a828e06c82a23769fc
john lester escarlan <jlescarlan11@gmail.com>
2024-10-05 09:18:22 +0800

    added omit utility type to add new pizza so that the id property of the pizza will be omitted.

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
+let cashInRegister = 100;
+let nextOrderId = 1;
+let nextPizzaId = 1;
+const orderQueue: Order[] = [];
+
 type Pizza = {
+  id: number;
   name: string;
   price: number;
 };
@@ -6,32 +12,33 @@ type Pizza = {
 type Order = {
   id: number;
   pizza: Pizza;
-  status: string;
+  status: "ordered" | "completed";
 };
 
-const menu = [
-  { name: "Margerita", price: 8 },
-  { name: "Pepperoni", price: 10 },
-  { name: "Hawaiian", price: 10 },
-  { name: "Veggie", price: 9 },
+const menu: Pizza[] = [
+  { id: nextPizzaId++, name: "Margerita", price: 8 },
+  { id: nextPizzaId++, name: "Pepperoni", price: 10 },
+  { id: nextPizzaId++, name: "Hawaiian", price: 10 },
+  { id: nextPizzaId++, name: "Veggie", price: 9 },
 ];
 
-let cashInRegister = 100;
-let nextOrderId = 1;
-const orderQueue: Order[] = [];
-
-function addNewPizza(pizzaObj: Pizza) {
-  menu.push(pizzaObj);
+function addNewPizza(pizzaObj: Omit<Pizza, "id">): Pizza {
+  const newPizza: Pizza = {
+    id: nextPizzaId++,
+    ...pizzaObj,
+  };
+  menu.push(newPizza);
+  return newPizza;
 }
 
-function placeOrder(pizzaName: string) {
+function placeOrder(pizzaName: string): Order | undefined {
   const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
   if (!selectedPizza) {
     console.error(`${pizzaName} does not exist in the menu`);
     return;
   }
   cashInRegister += selectedPizza.price;
-  const newOrder = {
+  const newOrder: Order = {
     id: nextOrderId++,
     pizza: selectedPizza,
     status: "ordered",
@@ -40,14 +47,30 @@ function placeOrder(pizzaName: string) {
   return newOrder;
 }
 
-function completeOrder(orderId: number) {
+function completeOrder(orderId: number): Order | undefined {
   const order = orderQueue.find((order) => order.id === orderId);
   if (!order) {
     console.error(`Order with id ${orderId} does not exist.`);
-    throw new Error();
+    return;
   }
   order.status = "completed";
   return order;
+}
+
+export function getPizzaDetail(identifier: string | number): Pizza | undefined {
+  if (typeof identifier === "string") {
+    const pizzaName = menu.find(
+      (pizza) => pizza.name.toLowerCase() === identifier.toLowerCase()
+    );
+    return pizzaName;
+  } else if (typeof identifier === "number") {
+    const pizzaId = menu.find((pizza) => pizza.id === identifier);
+    return pizzaId;
+  } else {
+    throw new TypeError(
+      "Parameter `identifier` must either a string or a number"
+    );
+  }
 }
 
 addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
